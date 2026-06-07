c34af1b73df98b602eb1291cfbf87e5f57c048f3
Stanleytowne <3065178273@qq.com>
2026-06-07 15:21:40 +0800

    feat: config

--- MemoryTest/temp.sh
+++ MemoryTest/temp.sh
@@ -1,3 +1,3 @@
-python MemoryTest/compare_update_capacity.py --merge-method concat
-python MemoryTest/compare_distractor_effect.py --merge-method concat
-python MemoryTest/compare_density_budget_effect.py --merge-method concat
+python MemoryTest/compare_update_capacity.py --merge-method sum
+python MemoryTest/compare_distractor_effect.py --merge-method sum
+python MemoryTest/compare_density_budget_effect.py --merge-method sum
